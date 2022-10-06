016d048d55d63312a9cac3240d606bb7d9ed0832
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2022-10-06 15:31:01 +0300

    fix: changed web address

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -16,7 +16,7 @@ const ProductList = () => {
       queryId
     }
 
-    fetch('https://192.168.0.2:8000', {
+    fetch('https://192.168.0.2:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
